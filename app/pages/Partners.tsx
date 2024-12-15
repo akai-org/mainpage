1e33b1b787bc1001990048527fd9420d916d3f5e
Beniamin Szawracki <45177027+benqSzaw@users.noreply.github.com>
2024-12-15 15:39:21 +0100

    feat: partners anim

--- app/pages/Partners.tsx
+++ app/pages/Partners.tsx
@@ -3,11 +3,8 @@ import { Element } from 'react-scroll';
 import { PARTNERS } from '@/app/resources/constants';
 import { useGSAP } from '@gsap/react';
 import { gsap } from 'gsap';
-import { useState } from 'react';
-import clsx from 'clsx';
 
 export default function Partners() {
-  const [isHref, setisHref] = useState(false);
   useGSAP(() => {
     const partners = document.querySelectorAll('#partner');
     const partners2 = document.querySelectorAll('#partner-2');
@@ -18,51 +15,59 @@ export default function Partners() {
         duration: 1,
         scrollTrigger: {
           trigger: partner,
-          start: 'top 50%',
-          end: 'bottom 50% ',
+          start: 'top 70%',
+          end: 'bottom 70% ',
         },
       });
-      partners2.forEach((partner, i) => {
-        gsap.to(partner, {
-          duration: 1,
-          borderWidth: 2,
-          scrollTrigger: {
-            trigger: partner,
-            start: 'top 50%',
-            end: 'bottom 50% ',
-          },
-          text: {
-            value: PARTNERS[i].name,
-          },
-          onComplete: () => setisHref(true),
-        });
+    });
+    partners2.forEach((partner, i) => {
+      gsap.to(partner, {
+        duration: 1,
+        borderWidth: 2,
+        scrollTrigger: {
+          trigger: partner,
+          start: 'top 70%',
+          end: 'bottom 70% ',
+        },
+      });
+    });
+    const partnerText = document.querySelectorAll('#partner-text');
+    partnerText.forEach((partnerText, i) => {
+      gsap.from(partnerText, {
+        opacity: 0,
+        duration: 1,
+        x: -100,
+        scrollTrigger: {
+          trigger: partnerText,
+          start: 'top 70%',
+          end: 'bottom 70% ',
+        },
       });
     });
   });
 
   return (
-    <Element
-      name="partners"
-      className="flex-center-col gap-5 px-5 sm:px-main md:gap-20"
-    >
-      <h1 className="text-3xl font-bold sm:text-5xl">Nasi partnerzy</h1>
-      <div className="flex-center-col flex w-full gap-10 md:flex-row md:justify-around md:gap-0">
+    <Element name="partners" className="flex-center gap-20 px-5 sm:px-main">
+      <div className="flex-center-col text-5xl font-bold">
+        {Array.from('Partnerzy').map((t, i) => (
+          <span key={i} id="partner-text">
+            {t.toUpperCase()}
+          </span>
+        ))}
+      </div>
+      <div className="flex-center-col flex gap-10">
         {PARTNERS.map((partner, i) => (
           <div
             key={i}
             id="partner"
             className="w-32 border-2 border-secondary p-1"
           >
-            <a
-              target="_blank"
-              href={partner.link}
-              className={clsx({ 'pointer-events-none': !isHref })}
-            >
+            <a target="_blank" href={partner.link}>
               <div
                 id="partner-2"
                 className="cursor-pointer select-none rounded border-secondary p-2 text-center"
               >
-                PARTNER
+                {partner.name}
               </div>
             </a>
           </div>
