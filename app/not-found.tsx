f68be9a5ed294dceff9de2a88dcc564e1b4a2eb7
Beniamin Szawracki <45177027+benqSzaw@users.noreply.github.com>
2024-12-15 15:21:28 +0100

    update:not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,65 @@
+'use client';
 import Link from 'next/link';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { useEffect, useRef } from 'react';
 
 export default function NotFound() {
+  const referenceRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, []);
+  useGSAP(() => {
+    if (referenceRef.current == null) return;
+    const elements = document.querySelectorAll('#not-found');
+    elements.forEach(element => {
+      gsap.to(element, {
+        x: -referenceRef.current!.clientWidth / 2,
+        ease: 'none',
+        duration: 10,
+        yoyo: true,
+        repeat: -1,
+      });
+    });
+  }, [referenceRef.current]);
   return (
-    <div className="flex-center-col z-50 h-dvh w-full gap-5 bg-red-500">
-      <h1 className="text-4xl font-bold sm:text-5xl">Not Found :(</h1>
-      <Link
-        href="/#top"
-        className="select-none border border-black p-1 duration-300 hover:invert active:bg-main"
-      >
-        Go to main page
-      </Link>
-    </div>
+    <>
+      {Array.from({ length: 3 }, (_, i1) => (
+        <div
+          key={i1}
+          id="not-found"
+          className="fixed z-40 flex w-full justify-between"
+          style={{ top: 200 * i1 }}
+        >
+          {Array.from({ length: 10 }, (_, i2) => (
+            <div key={i2} className="w-fit flex-shrink-0 px-5 text-[150px]">
+              {i2 % 2 == 0 ? (
+                404
+              ) : (
+                <>
+                  <span className="text-secondary">A</span>KAI
+                </>
+              )}
+            </div>
+          ))}
+        </div>
+      ))}
+
+      <div ref={referenceRef} className="flex-center mt-auto h-dvh w-full">
+        <div className="flex-center-col z-50 mx-auto gap-5 rounded border-4 border-red-500 bg-white p-5">
+          <h1 className="text-4xl font-bold sm:text-5xl">Not Found :(</h1>
+          <Link
+            href="/#top"
+            className="select-none border border-red-500 p-1 duration-300 hover:border-black hover:underline active:bg-main"
+          >
+            Go to main page
+          </Link>
+        </div>
+      </div>
+    </>
   );
 }
