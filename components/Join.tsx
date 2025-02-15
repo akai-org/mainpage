911daa758452a4a6e303e0f8204580cf9ba100a4
Beniamin Szawracki <45177027+benqSzaw@users.noreply.github.com>
2025-02-16 00:46:25 +0100

    feat: translation refactor

--- components/Join.tsx
+++ components/Join.tsx
@@ -6,15 +6,20 @@ import {
   Heading,
   Text,
 } from '@radix-ui/themes';
-import { JOBS, MAIL } from '@/resources/constants';
+import { MAIL } from '@/resources/constants';
 import { TextAnimate } from '@/components/ui/TextAnimate';
 import { useTranslation } from 'react-i18next';
-import { useCurrentLocale } from 'next-i18n-router/client';
-import { i18nConfig } from '@/resources/i18n';
+
+type JobItemT = {
+  name: string;
+  desc: string;
+};
 
 export default function Join() {
   const { t } = useTranslation('join');
-  const locale = useCurrentLocale(i18nConfig);
+  const jobs = t<string, { returnObjects: true }, JobItemT[]>('jobs', {
+    returnObjects: true,
+  });
 
   return (
     <div className="container-text flex-center-col border-main p-container gap-5 border-y pb-0">
@@ -22,14 +27,14 @@ export default function Join() {
       <Text className="md:w-1/2">{t('text')} </Text>
       <div className="border-main mb-5 !w-full border sm:!w-1/2" />
       <div className="flex-center-col gap-16">
-        {JOBS.map((job, i) => (
+        {jobs.map((job, i) => (
           <div key={i} className="flex flex-col gap-5 md:w-1/2">
             <Code className="w-fit">
               <TextAnimate animation="slideLeft" by="character">
                 {job.name}
               </TextAnimate>
             </Code>
-            <Blockquote>{locale == 'en' ? job.descEn : job.desc}</Blockquote>
+            <Blockquote>{job.desc}</Blockquote>
           </div>
         ))}
       </div>
--- resources/constants.tsx
+++ resources/constants.tsx
@@ -6,33 +6,6 @@ export const APPS_LINK = 'https://apps.akai.org.pl';
 export const GITHUB_API = 'https://api.github.com/users/akai-org';
 export const CAT_LINK = 'https://cat.put.poznan.pl';
 
-export const JOBS = [
-  {
-    name: 'Front-end development',
-    desc: 'Wszystko to, co widzi użytkownik. Odpowiada za wygląd i zachowanie strony lub aplikacji internetowej. Powstaje przy pomocy HTML, CSS i JavaScript, ale w dzisiejszych czasach często nie jest to wystarczające. Dlatego często korzystamy z różnych frameworków jak React, Vue, Next.js i Gatsby.',
-    descEn:
-      "Everything the user sees. Responsible for the appearance and behavior of a website or web application. It is created with the help of HTML, CSS, and JavaScript, but nowadays, that's often not enough. That's why we often use various frameworks like React, Vue, Next.js, and Gatsby.",
-  },
-  {
-    name: 'Back-end development',
-    desc: 'Oprogramowanie uruchamiane po stronie serwera. Przetwarza dane i wykonuje różne operacje. To tutaj dzieje się magia. W kole korzystamy z różnych języków programowania (m. in. Python, Java, PHP, Go, Ruby)',
-    descEn:
-      'Software that runs on the server side. It processes data and performs various operations. This is where the magic happens. In the circle, we use different programming languages (e.g., Python, Java, PHP, Go, Ruby).',
-  },
-  {
-    name: 'Mobile development',
-    desc: 'Świat aplikacji mobilnych, dostępnych na wyciągnięcie ręki, na urządzeniach z systemami iOS i Android. Aplikacje piszemy najczęściej natywnie dla Androida w języku Kotlin lub multiplatform przy użyciu Fluttera. W ramach sekcji mobile tworzymy także czasem PWA.',
-    descEn:
-      'The world of mobile applications, available at your fingertips, on iOS and Android devices. We typically write apps natively for Android in Kotlin or multiplatform using Flutter. In the mobile section, we also sometimes create PWAs.',
-  },
-  {
-    name: 'Graphic design / UI / UX',
-    desc: 'Zanim coś stworzymy, warto to zaprojektować. Oprócz tego, żeby było przyjemne dla oka - warto również zadbać o to, aby interfejs był intuicyjny dla użytkownika.',
-    descEn:
-      "Before we create something, it's worth designing it. In addition to making it visually appealing, it's also important to ensure that the interface is intuitive for the user.",
-  },
-];
-
 export const PARTNERS = [
   {
     name: 'AppUnite',
@@ -63,22 +36,18 @@ export const PARTNERS = [
 
 export const SOCIALS = [
   {
-    name: 'Facebook',
     link: 'https://www.facebook.com/akai.pp',
     icon: <FaFacebook />,
   },
   {
-    name: 'GitHub',
     link: 'https://github.com/akai-org',
     icon: <FaGithub />,
   },
   {
-    name: 'X',
     link: 'https://x.com/akai_pp',
     icon: <FaX />,
   },
   {
-    name: 'LinkedIn',
     link: 'https://www.linkedin.com/company/akai-pp',
     icon: <FaLinkedin />,
   },
